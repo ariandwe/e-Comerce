61b5f450cd3dd85fb334c687197c06519777f1cd
ariandwe <arian5770657@gmail.com>
2022-10-13 22:19:15 -0500

    0.1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.contenedor-modal .overlay').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		ev.stopPropagation();
+		this.parentNode.classList.add('active');
+	});
+});
+
+document.querySelectorAll('.contenedor-modal ').forEach((el) => {
+	el.addEventListener('click', function (ev) {
+		this.classList.remove('active');
+	});
+});
